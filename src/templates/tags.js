198eb09a6cc34c3499bd2f9cc340167b52a6ceb3
Liviu Iancuta <liviu.iancuta@gmail.com>
2020-04-17 10:12:07 +0300

    Translate page

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -16,9 +16,9 @@ class TagRoute extends React.Component {
     const tag = this.props.pageContext.tag
     const title = this.props.data.site.siteMetadata.title
     const totalCount = this.props.data.allMarkdownRemark.totalCount
-    const tagHeader = `${totalCount} post${
-      totalCount === 1 ? '' : 's'
-      } tagged with “${tag}”`
+    const tagHeader = `${totalCount} ${
+      totalCount === 1 ? 'postare' : 'postări'
+      } etichetate cu “${tag}”`
 
     return (
       <Layout>
@@ -36,7 +36,7 @@ class TagRoute extends React.Component {
                 <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
                 <ul className="taglist">{postLinks}</ul>
                 <p>
-                  <Link to="/tags/">Browse all tags</Link>
+                  <Link to="/tags/">Răsfoiește toate etichetele</Link>
                 </p>
               </div>
             </div>
